8acc4d989a9786024f2751b023d5a6554cc5628c
sungyotkim <sungyotkim@gmail.com>
2022-08-09 12:16:03 -0400

    set up board

--- chess_part_one/board.rb
+++ chess_part_one/board.rb
@@ -1,3 +1,13 @@
-class Board
+require_relative "piece.rb"
 
+class Board
+    def initialize
+        @board = Array.new(8) { Array.new(8, nil) }
+        (0..7).each do |col|
+            board[1][col] = Piece.new #pawns
+            board[6][col] = Piece.new #pawns
+        end
+
+        
+    end
 end
